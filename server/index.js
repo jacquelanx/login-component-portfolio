0467e9ea97336a7dde44fe6f60587aa07b382e78
jacquelanx <109934084+jacquelanx@users.noreply.github.com>
2025-07-22 12:45:55 -0500

    Initial setup for frontend & backend

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Set up server
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+// MongoDB setup
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('🌸 MongoDB connected!'))
+    .catch(err => console.error(err));
+
+// Run server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🎀 Server is running on Port ${PORT}!`));
